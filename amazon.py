cface9862c07a7d8c281a5ac47a0708bde5e116b
Ximer - Marco <20438890+Ximer612@users.noreply.github.com>
2024-11-23 16:29:32 +0100

    Replaced "Product" with item_name variable

--- amazon.py
+++ amazon.py
@@ -17,8 +17,8 @@ def check(AMAZON_PRODUCT_LINK,BOT_TOKEN,BOT_CHATID):
     seller_name = str(soup_seller_name)
     item_name = str(soup_item_name.contents[0]).strip()
 
-    if  seller_name == 'None' or seller_name is None or seller_name == "Amazon":
+    if  seller_name == 'None' or seller_name is None or seller_name == 'Amazon':
         bot_send_text(item_name+" is now sold by amazon!",BOT_TOKEN,BOT_CHATID)
-        print("\x1b[42m"+ "Product is now sold by amazon!"+ "\x1b[0m")
+        print("\x1b[42m"+ item_name+" is now sold by amazon!"+ "\x1b[0m")
     else:
-        print("\x1b[41m"+"Product is sold by "+soup_seller_name.next+ "!\x1b[0m")
+        print("\x1b[41m"+item_name+" is sold by "+soup_seller_name.next+ "!\x1b[0m")
